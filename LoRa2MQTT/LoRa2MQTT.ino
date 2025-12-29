14f7887d8bc5241d26cc82e21f0348bcab412b52
Jay Han <jayhan.name@gmail.com>
2025-12-29 17:02:58 +0100

    V2 Cellar only

--- LoRa2MQTT/LoRa2MQTT.ino
+++ LoRa2MQTT/LoRa2MQTT.ino
@@ -5,14 +5,10 @@
 #include <MQTT.h>
 #include <Esp.h>
 
-#define VERSION      "v100"
-char header[30] =    "";
+#define VERSION      "v200"
 
 #define PIN_LED      15
 #define PIN_BUTTON   0
-bool screenOn = true;
-int buttonState;
-unsigned long screensaver;
 
 #define LORA_RST     39
 #define LORA_SCK     37
@@ -45,9 +41,8 @@ MQTTClient mqttClient(1024);
 #define LCD_H_RES     128
 #define LCD_V_RES     64
 #define LCD_PAGES     (LCD_V_RES / 8)
-#define LINE_WIDTH    (LCD_H_RES / FONT_PITCH)
+#define LINE_WIDTH    (LCD_H_RES / FONT_PITCH / 2)
 #include "fontdata.h"
-#define CHAR_RSSI     0x01
 
 const unsigned char ssd1306_init[] = {
     0xAE | 0x00,          // SET_DISP            off
@@ -71,24 +66,6 @@ void ssd1306_cmd(unsigned char cmd) {
     Wire.write(0x80);
     Wire.write(cmd);
     Wire.endTransmission();
-}
-
-void displayOn() {
-    ssd1306_cmd(0x81);
-    ssd1306_cmd(0xFF);
-    Serial.println("Screen ON");
-    screenOn = true;
-    digitalWrite(PIN_LED, !screenOn);
-    screensaver = millis();
-}
-
-void displayOff() {
-    ssd1306_cmd(0x81);
-    ssd1306_cmd(0x00);
-    Serial.println("Screensaver");
-    screenOn = false;
-    digitalWrite(PIN_LED, !screenOn);
-    buttonState = digitalRead(PIN_BUTTON);
 }
 
 void sendPagePos(int page, int pos, unsigned char *thisBuffer, int length) {
@@ -119,43 +96,57 @@ void initDisplay() {
     } else {
         Serial.println("I2C OLED failed");
     }
-
-    screenOn = true;
-    screensaver = millis();
 }
 
-unsigned char line_top[LCD_H_RES];
-unsigned char line_bot[LCD_H_RES];
+byte nybble_low(byte original) {
+    byte result = 0;
+    for (int pos = 0; pos < 4; pos++) {
+        if (original & (1 << pos)) result |= (3 << (pos * 2));
+    }
+    return result;
+}
 
-void writeLine(int line, char *text) {
+byte nybble_hi(byte original) {
+    byte result = 0;
+    for (int pos = 0; pos < 4; pos++) {
+        if (original & (1 << (4 + pos))) result |= (3 << (pos * 2));
+    }
+    return result;
+}
+
+unsigned char lines[4][LCD_H_RES];
+
+void writeBig(int line, char *text) {
     int target = 0;
     int i;
 
     for (i = 0; i < strlen(text) && i < LINE_WIDTH; i++) {
         int index = text[i];
         for (int col = 0; col < FONT_WIDTH_16; col++) {
-            line_top[target] = fontdata_top[index][col];
-            line_bot[target] = fontdata_bot[index][col];
+            lines[0][target] = nybble_low(fontdata_top[index][col]);
+            lines[1][target] = nybble_hi(fontdata_top[index][col]);
+            lines[2][target] = nybble_low(fontdata_bot[index][col]);
+            lines[3][target] = nybble_hi(fontdata_bot[index][col]);
+            target++;
+            lines[0][target] = nybble_low(fontdata_top[index][col]);
+            lines[1][target] = nybble_hi(fontdata_top[index][col]);
+            lines[2][target] = nybble_low(fontdata_bot[index][col]);
+            lines[3][target] = nybble_hi(fontdata_bot[index][col]);
             target++;
         }
-        line_top[target] = line_bot[target] = 0x00;
+        lines[0][target] = lines[1][target] = lines[2][target] = lines[3][target] = 0x00;
+        target++;
+        lines[0][target] = lines[1][target] = lines[2][target] = lines[3][target] = 0x00;
         target++;
     }
-    for (; i < LINE_WIDTH; i++) {
-        for (int col = 0; col <= FONT_WIDTH_16; col++) {
-            line_top[target] = line_bot[target] = 0x00;
-            target++;
-        }
-        line_top[target] = line_bot[target] = 0x00;
-        target++;
+    
+    for (; target < LCD_H_RES; target++) {
+        lines[0][target] = lines[1][target] = lines[2][target] = lines[3][target] = 0x00;
     }
 
-    sendPagePos(line * 2,     0, line_top, target);
-    sendPagePos(line * 2 + 1, 0, line_bot, target);
-}
-
-void writeHeader(char *header) {
-    writeLine(0, header);
+    for (int sub = 0; sub < 4; sub++) {
+        sendPagePos(line * 4 + sub, 0, lines[sub], target);
+    }
 }
 
 // --------------------------------
@@ -164,71 +155,35 @@ void writeHeader(char *header) {
 #define MAX_PAYLOAD  256
 
 #define ID_HEADER    0x92
-#define SOURCES      3
-#define ID_BAL       0
-#define ID_CELLAR    1
-#define ID_TEST      2
-float parseTest(int length, byte *payload, char *message, char *topic, char *json);
-float parseBal(int length, byte *payload, char *message, char *topic, char *json);
-float parseCellar(int length, byte *payload, char *message, char *topic, char *json);
+#define ID_CELLAR    0xCE
 
 struct {
-    byte code;
-    char name[10];
-    float (*parser)(int, byte*, char*, char*, char*);
-} Vector[SOURCES] = {
-    {0xBA, "Bal",    parseBal},
-    {0xCE, "Cellar", parseCellar},
-    {0xA5, "Test",   parseTest}
-};
-
-struct {
-    char message[1024];
-    char topic[128];
+    char message[20];
     char json[256];
-    unsigned long millis;
     float battery;
     int rssi;
     float snr;
-} Rx[SOURCES];
+} Rx;
 byte payload[MAX_PAYLOAD];
 
-int onScreen = -1;
-void sendMessage(int source) {
-    char topic[20], info[20];
+#define HASS_TOPIC  "homeassistant/sensor/CellLora/state"
+#define HASS_JSON   "{\"temperature\": %.1f, \"humidity\": %.0f, \"voltage\": %.1f}"
 
-    onScreen = source;
-    sprintf(info, "[%d,%.2f]", Rx[source].rssi, Rx[source].snr);
-    sprintf(topic, "%s(0m)", Vector[source].name);
-    Serial.printf("%s:%s %s\n", topic, Rx[source].message, info);
-    writeLine(1, topic);
-    writeLine(2, Rx[source].message);
-    writeLine(3, info);
-
-    mqttClient.publish(topic, Rx[source].message);
-    if (Rx[source].json[0] != 0) {
-        mqttClient.publish(Rx[source].topic, Rx[source].json);
-    }
-    
-    char rssi;
-    if (Rx[source].rssi > 135) rssi = 1;
-    else if (Rx[source].rssi < 55) rssi = 7;
-    else rssi = 7 - (Rx[source].rssi - 55) / 16;
-
-    Rx[source].message[0] = 0;
+void sendMessage() {
+    writeBig(1, Rx.message);
+    mqttClient.publish(MQTT_TOPIC, Rx.message);
+    mqttClient.publish(HASS_TOPIC, Rx.json);
     mqttClient.loop();
 }
 
 void skipMessage(bool reboot) {
     while(LoRa.available() > 0) LoRa.read();
-    digitalWrite(PIN_LED, !screenOn);
     if (reboot) ESP.restart();
 }
 
 // CONTINUOUS, this is called from ISR!!!
+bool isReceived = false;
 void onReceive(int packetSize) {
-    digitalWrite(PIN_LED, screenOn);
-    
     if (packetSize == 0 || packetSize > MAX_PAYLOAD) {
         skipMessage(true);
         return;
@@ -241,74 +196,45 @@ void onReceive(int packetSize) {
     }
     
     byte senderCode = LoRa.read();
+    if (senderCode != ID_CELLAR) {
+        skipMessage(true);
+        return;
+    }
+    
     int length = LoRa.available();
     if (length != packetSize - 2) {
         skipMessage(true);
         return;
     }
     
-    int source;
-    for (source = 0; source < SOURCES; source++) {
-        if (senderCode == Vector[source].code) {
-            if (Rx[source].message[0] != 0) {
-                skipMessage(true);
-                return;
-            }
-
-            Rx[source].millis = millis();
-            for (int i = 0; i < length; i++) {
-                payload[i] = LoRa.read();
-            }
-            Rx[source].json[0] = 0;
-            float battery = Vector[source].parser(length, payload, Rx[source].message, Rx[source].topic, Rx[source].json);
-            if (battery == 0.0) {
-                skipMessage(true);
-                return;
-            }
-            Rx[source].battery = battery;
-            Rx[source].rssi = -LoRa.packetRssi();
-            Rx[source].snr  = LoRa.packetSnr();
-            break;
-        }
+    for (int i = 0; i < length; i++) {
+        payload[i] = LoRa.read();
     }
     
-    if (source == SOURCES) {
-        skipMessage(true);
-        return;
-    }
-
-    skipMessage(false);
-}
-
-float parseTest(int length, byte *payload, char *message, char *topic, char *json) {
-    memcpy(message, payload, length);
-    message[length] = 0;
-    return 4.2;
-}
-
-float parseBal(int length, byte *payload, char *message, char *topic, char *json) {
-    char mail = payload[0] == 1 ? 'Y' : 'N';
-    float battery = (float)payload[1] / 10.0;
-    sprintf(message, "mail=%c batt=%3.1fV", mail, battery);
-    return battery;
-}
-
-#define HASS_TOPIC   "homeassistant/sensor/CellLora/state"
-#define HASS_MESSAGE "{\"temperature\": %.1f, \"humidity\": %.0f, \"voltage\": %.1f}"
-
-float parseCellar(int length, byte *payload, char *message, char *topic, char *json) {
     float battery = (float)payload[0] / 10.0;
     float temperature = (float)payload[1] + (float)payload[2] / 10.0;
     float humidity = (float)payload[3];
 
-    if (battery > 5.0 || battery < 2.0) return 0.0;
-    if (temperature > 50.0 || temperature <= 0.0) return 0.0;
-    if (humidity >= 100.0 || humidity <= 10.0) return 0.0;
+    isReceived = false;
+    do {
+        if (battery > 5.0 || battery < 2.0) break;
+        if (temperature > 50.0 || temperature <= 0.0) break;
+        if (humidity >= 100.0 || humidity <= 10.0) break;
     
-    sprintf(message, "%3.1fV %.1fC %.0f%%", battery, temperature, humidity);
-    strcpy(topic, HASS_TOPIC);
-    sprintf(json, HASS_MESSAGE, temperature, humidity, battery);
-    return battery;
+        sprintf(Rx.message, "%.1f %.0f", battery, temperature, humidity);
+        sprintf(Rx.json, HASS_JSON, temperature, humidity, battery);
+        
+        Rx.battery = battery;
+        Rx.rssi = -LoRa.packetRssi();
+        Rx.snr  = LoRa.packetSnr();
+    
+        skipMessage(false);
+        isReceived = true;
+    } while(false);
+    
+    if (!isReceived) {
+        skipMessage(true);
+    }
 }
 
 // -----------------
@@ -323,11 +249,11 @@ void setup() {
     pinMode(PIN_LED, OUTPUT);
     digitalWrite(PIN_LED, LOW);
     pinMode(PIN_BUTTON, INPUT_PULLUP);
-    buttonState = digitalRead(PIN_BUTTON);
     
     WiFi.begin(SSID, PASS);
 
     initDisplay();
+    writeBig(0, "OK");
     
     pinMode(LORA_RST, OUTPUT);
     digitalWrite(LORA_RST, HIGH);
@@ -362,8 +288,10 @@ void setup() {
             ESP.restart();
         }
     }
-    strcat(header, (char*)WiFi.localIP().toString().c_str());
-    Serial.println(header);
+    
+    char ip[30] = "";
+    strcat(ip, (char*)WiFi.localIP().toString().c_str());
+    Serial.println(ip);
 
     mqttClient.begin(MQTT_SERVER, wifiClient);
     mqttClient.setKeepAlive(3600);
@@ -372,65 +300,36 @@ void setup() {
         delay(1000);
         ESP.restart();
     }
-    mqttClient.publish(MQTT_TOPIC, header, true, 0);
+    mqttClient.publish(MQTT_TOPIC, ip, true, 0);
 
     Serial.println("MQTT connected");
-    writeHeader(header);
 
     Serial.println("Listening...");
     LoRa.onReceive(onReceive);
     LoRa.receive();
 }
 
-unsigned long millisElapsed(unsigned long end, unsigned long start) {
-    if (end >= start) return (end - start);
-    unsigned long inverse = start - end - 1;
-    unsigned long result = 4294967295L - inverse;
-    return result;
-}
-
 bool messageSent = false;
 void loop() {
-   static unsigned long lastUpdate_ms = millis();
+    char elapsed[8];
     
-    for (int source = 0; source < SOURCES; source ++) {
-        if (Rx[source].message[0] != 0) {
-            sendMessage(source);
-            messageSent = true;
-        }
-    }
-
-    if (onScreen >= 0) {
-        unsigned long seconds;
-        char elapsedTime[8];
-        if (millisElapsed(millis(), lastUpdate_ms) > 1000L * 60) {
-            seconds = millisElapsed(millis(), Rx[onScreen].millis) / 1000;
-            if (seconds >= 3600 * 24 * 99) {
-                strcpy(elapsedTime, "Inf");
-            } else if (seconds >= 3600 * 24) {
-                sprintf(elapsedTime, "%dd", seconds / 3600 / 24);
-            } else if (seconds >= 3600) {
-                sprintf(elapsedTime, "%dh", seconds / 3600);
-            } else {
-                sprintf(elapsedTime, "%dm", seconds / 60);
-            }
-            
-            char topic[20];
-            sprintf(topic, "%s(%s)", Vector[onScreen].name, elapsedTime);
-            writeLine(1, topic);
-        
-            lastUpdate_ms = millis();
-        }
-    }
-    
-    if (screenOn) {
-        if (millisElapsed(millis(), screensaver) > 1000L * 30) {
-            displayOff();
+    int minutes = millis() / 60000;
+    if (minutes > 60) {
+        int hours = minutes / 60;
+        if (hours > 24) {
+            int days = hours / 24;
+            if (days >= 100) ESP.restart();
+            sprintf(elapsed, "%dd%hh", days, hours);
+        } else {
+            sprintf(elapsed, "%dh%02dm", hours, minutes);
         }
     } else {
-        if (buttonState != digitalRead(PIN_BUTTON)) {
-            displayOn();
-        }
+        sprintf(elapsed, "%dm", minutes);
+    }
+    writeBig(0, elapsed);
+    
+    if (isReceived) {
+       sendMessage();
     }
 
     if (!mqttClient.connected()) {
@@ -439,7 +338,7 @@ void loop() {
       ESP.restart();
     }
     mqttClient.loop();
-
     sleep(1);
-    if (messageSent) ESP.restart();
+
+    if (isReceived) ESP.restart();
 }
