510605a8536d97b64cb1098327a11b6f38f5a2f5
Jay Han <jayhan.name@gmail.com>
2026-03-29 18:28:55 +0200

    configuration for recorder

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-mosquitto_pub -t homeassistant/sensor/CellLora_Temperature/config -f temp.config
-mosquitto_pub -t homeassistant/sensor/CellLora_Humidity/config -f humi.config
-mosquitto_pub -t homeassistant/sensor/CellLora_Battery/config -f batt.config
+mosquitto_pub -u guest -P invitation -t homeassistant/sensor/CellLora_Temperature/config -f temp.config
+mosquitto_pub -u guest -P invitation -t homeassistant/sensor/CellLora_Humidity/config -f humi.config
+mosquitto_pub -u guest -P invitation -t homeassistant/sensor/CellLora_Battery/config -f batt.config
